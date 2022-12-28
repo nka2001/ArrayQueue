1115e24c526e4ce0810fea72d1238e3192533bc9
nka2001 <89935053+nka2001@users.noreply.github.com>
2022-12-27 19:08:29 -0500

    ArrayQueue - GUI Complete

--- src/main/java/com/mycompany/arrayqueue/PrimaryController.java
+++ src/main/java/com/mycompany/arrayqueue/PrimaryController.java
@@ -1,12 +1,29 @@
 package com.mycompany.arrayqueue;
 
 import java.io.IOException;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 
 public class PrimaryController {
 
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+    private ListView<String> listview;
+    @FXML
+    private TextField AddTextField;
+    @FXML
+    private Button AddButton;
+    @FXML
+    private Button RemoveButton;
+
+    
+    @FXML
+    private void ExecuteAdd(ActionEvent event) {
+    }
+
+    @FXML
+    private void ExecuteRemove(ActionEvent event) {
     }
 }
